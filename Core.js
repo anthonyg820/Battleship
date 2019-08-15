25620f5365f1d023cc3b572ced5564bc7683bdb9
Ant <44145630+anthonyg820@users.noreply.github.com>
2019-08-15 19:53:03 -0400

    Added Core.js. Modified Splash.html.

---
+++ Core.js
@@ -0,0 +1,4 @@
+
+//Get the height and width of the user's browser
+var screenHeight = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+var screenWidth = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
