c77661d7f2fe8f89e08379f6f854c31631f5d11f
Ant <44145630+anthonyg820@users.noreply.github.com>
2019-08-15 22:58:54 -0400

    I've almost finished the styling of the splash page.

--- Core.js
+++ Core.js
@@ -2,3 +2,10 @@
 //Get the height and width of the user's browser
 var screenHeight = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
 var screenWidth = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+
+function setContentWrapperSize()
+{
+    var contentWrapper = document.getElementById("content-wrapper");
+    contentWrapper.style.width = screenWidth + "px";
+    contentWrapper.style.height = screenHeight + "px";
+}
