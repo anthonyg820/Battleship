9d3e58bbb9743566c65ed7ab6d85b8b0b76f0df4
Ant <44145630+anthonyg820@users.noreply.github.com>
2019-08-16 22:40:44 -0400

    I've added all 6 battleships to the game grid. I've implemented drag-and-drop logic for all of them. Next, I will work on collisions (overlapping ships)

--- Game.js
+++ Game.js
@@ -1,45 +1,98 @@
 //This array is used to convert the horizontal letter cells to numerical values for calculating positions
 var horizPosToNums = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"];
 
-/*function moveShip(targetID)
+
+/*
+    Implementing MVC
+    Model - 
+    View - Informs the user of information (hit/miss)
+    Controller - 
+*/
+var model =
 {
-    var clickedShip = document.getElementById(targetID);
-    var currentX = 0;
-    var shipParent = clickedShip.parentElement;
-    //alert(shipParent.className);
-    var xCellLetter = shipParent.className.substring(0,1);
-    //alert(xCellLetter);
+    gridSize: 10,
+    numOfShips: 6,
+    shipsSunkPlayer: 0,
+    shipsPlayer: [{id: 'oneShip_h', locations: [null], hit: [false]},
+            {id: 'oneShip_v', locations: [null], hit: [false]},
+            {id: 'twoShip_h', locations: [null, null], hit: [false, false]},
+            {id: 'twoShip_v', locations: [null, null], hit: [false, false]},
+            {id: 'threeShip_h', locations: [null, null, null], hit: [false, false, false]},
+            {id: 'threeShip_v', locations: [null, null, null], hit: [false, false, false]}],
 
-    var xCellNum = horizPosToNums.indexOf(xCellLetter) + 1;
-    //alert(xCellNum);
+    shipsOpponent: [{id: 'oneShip_h', locations: [null], hit: [false]},
+            {id: 'oneShip_v', locations: [null], hit: [false]},
+            {id: 'twoShip_h', locations: [null, null], hit: [false, false]},
+            {id: 'twoShip_v', locations: [null, null], hit: [false, false]},
+            {id: 'threeShip_h', locations: [null, null, null], hit: [false, false, false]},
+            {id: 'threeShip_v', locations: [null, null, null], hit: [false, false, false]}],
     
-    var yCellNum = shipParent.className.substring(1);
-
-    var xMouseInitial = event.clientX; 
-    var xMouse = xMouseInitial; 
-    console.log(xMouse);
-
-    var curCell = document.getElementsByClassName(xCellLetter + yCellNum)[0];
-
-    document.onmousemove = function()
-    {
-        xMouse = event.clientX;
-        console.log(xMouse);
-
-        if((xMouse - xMouseInitial) > 30)
+    fire: function(guess){
+        for(var i = 0; i < this.shipsPlayer.length; i++)
         {
-            curCell.removeChild(curCell.childNodes[0]);
-
-            var newCell = document.getElementsByClassName(horizPosToNums[xCellNum] + yCellNum)[0];
-            newCell.appendChild(clickedShip);
-            return;
+            var curShip = ships[i];
+            var hitIndex = curShip.locations.indexOf(guess);
+            if(hitIndex > 0)
+            {
+                curShip.hit[hitIndex] = true;
+                view.showHit(guess);
+            }
+            else
+            {
+                view.showMiss(guess);
+            }
         }
+    },
+
+    isSunk: function(ship){
+        for(var i = 0; i < ship.length; i++)
+        {
+            if(!ship.hits[i])
+                return false;
+        }
+        return true;
     }
+}
 
-    //alert("The ID clicked is: " + targetID + ".\n The current x-pos is: " + currentX);
-}*/
+var view = 
+{
+    showHit: function(cellNumber){
+        var cell = document.getElementById("playerTable").getElementsByClassName(cellNumber)[0];
+        cell.style.background = "#";
+    },
+    showMiss: function(cellNumber){
+        var cell = document.getElementById("playerTable").getElementsByClassName(cellNumber)[0];
+        cell.style.background = "#";
+    },
+    showShipSunk: function(){
+        
+    }
+}
 
-function moveShip(targetID)//Triggers using onmousedown event
+var controller =
+{
+    guessAmount: 0,
+
+    activatePlayerBoard: function(){
+
+    },
+
+    activateOpponentBoard: function(){
+
+    },
+
+    startGame: function(){
+        this.activatePlayerBoard();
+        this.activateOpponentBoard();
+    },
+
+    handleGuess: function(guess){
+        model.fire(guess);
+        this.guessAmount++;
+    }
+}
+
+function moveShipHorizontal(targetID, shipLength)//Triggers using onmousedown event
 {
     var clickedShip = document.getElementById(targetID);
     var shipParent = clickedShip.parentElement;
@@ -48,7 +101,7 @@ function moveShip(targetID)//Triggers using onmousedown event
 
     //Create a ship copy which will be used to update the actual ship position
     var copyOfClickedShip = clickedShip.cloneNode( true ); 
-    copyOfClickedShip.setAttribute( 'id', "cloneShip" );
+    copyOfClickedShip.setAttribute( 'id', targetID);
     copyOfClickedShip.setAttribute( 'class', "shipBlock threeShip" );
     copyOfClickedShip.style.display = "block";
     copyOfClickedShip.onmousedown = null;
@@ -79,7 +132,7 @@ function moveShip(targetID)//Triggers using onmousedown event
         console.log("x: " + xMouseCurrent + "\n Y: " + yMouseCurrent + "\nCurrent Cell: " + curCell.className + "\nXNUM: " + xCellNum + "\nYNUM: " + yCellNum + "\n cellRead: " + (horizPosToNums[xCellNum - 2] + yCellNum));
         
         //Track x-movement
-        if(((xMouseCurrent - xMouseInitial) > 30) && xCellNum < 8)//If moving right
+        if(((xMouseCurrent - xMouseInitial) > 30) && xCellNum < 10-shipLength+1)//If moving right
         {
             xMouseInitial = xMouseCurrent;
             var newCell = document.getElementsByClassName(horizPosToNums[xCellNum] + yCellNum)[0];
@@ -121,14 +174,155 @@ function moveShip(targetID)//Triggers using onmousedown event
     {
         document.onmousemove = null;
         copyOfClickedShip.parentNode.removeChild(copyOfClickedShip);
-        curCell.appendChild(clickedShip);
-        document.getEl
         clickedShip.style.display = "block";
+        if(curCell.className != shipParent.className)
+        {
+            curCell.appendChild(clickedShip);
+            curCellLetter = curCell.className.substring(0, 1);
+            curCellNum = parseInt(curCell.className.substring(1));
+        }
+        else
+        {
+            shipParent.appendChild(clickedShip);
+        }
+        
+        //Set the ship positions within Model.ships
+        var ship;
+        for(var i = 0; i < model.shipsPlayer.length; i++)
+        {
+            var curShip = model.shipsPlayer[i];
+            if(curShip.id == targetID)
+            {
+                ship = curShip;
+                break;
+            }
+        }
+
+        for(var i = 0; i < shipLength; i++)
+        {
+            ship.locations[i] = curCellLetter + curCellNum;
+            curCellLetter = horizPosToNums[horizPosToNums.indexOf(curCellLetter)+1];
+        }
+
+        console.log(model.shipsPlayer);
     }
 }
 
-function stopMoveShip() //Triggers using onmouseup event
+function moveShipVertical(targetID, shipLength)//Triggers using onmousedown event
 {
-    document.onmousemove = null;
+    var clickedShip = document.getElementById(targetID);
+    var shipParent = clickedShip.parentElement;
+
+    clickedShip.style.display = "none";
+
+    //Create a ship copy which will be used to update the actual ship position
+    var copyOfClickedShip = clickedShip.cloneNode( true ); 
+    copyOfClickedShip.setAttribute( 'id', targetID);
+    copyOfClickedShip.setAttribute( 'class', "shipBlock threeShip" );
+    copyOfClickedShip.style.display = "block";
+    copyOfClickedShip.onmousedown = null;
+    shipParent.appendChild(copyOfClickedShip);
+
+    //Get current cell coordinates
+    var xCellLetter = shipParent.className.substring(0,1);
+    var xCellNum = horizPosToNums.indexOf(xCellLetter) + 1; //Convert the letter position to its number position
+
+    var yCellNum = parseInt(shipParent.className.substring(1));
+
+    var curCell = shipParent.cloneNode(true);
+    curCell.setAttribute('id', 'cloneCell');//This keeps track of the cell the ship is in while being moved
+
+    //Establish initial and current coordinate values
+    var xMouseInitial = event.clientX; 
+    var xMouseCurrent = xMouseInitial; 
+
+    var yMouseInitial = event.clientY; 
+    var yMouseCurrent = yMouseInitial; 
+    console.log("x: " + xMouseCurrent + "\n Y: " + yMouseCurrent + "\nCurrent Cell: " + curCell.className + "\nXNUM: " + xCellNum + "\nYNUM: " + yCellNum);
+
+    //Begin movement
+    document.onmousemove = function()
+    {
+        xMouseCurrent = event.clientX;
+        yMouseCurrent = event.clientY;
+        console.log("x: " + xMouseCurrent + "\n Y: " + yMouseCurrent + "\nCurrent Cell: " + curCell.className + "\nXNUM: " + xCellNum + "\nYNUM: " + yCellNum + "\n cellRead: " + (horizPosToNums[xCellNum - 2] + yCellNum));
+        
+        //Track x-movement
+        if((xMouseCurrent - xMouseInitial) > 30)//If moving right
+        {
+            xMouseInitial = xMouseCurrent;
+            var newCell = document.getElementsByClassName(horizPosToNums[xCellNum] + yCellNum)[0];
+            newCell.appendChild(copyOfClickedShip);
+            xCellNum++;
+            curCell = document.getElementsByClassName(horizPosToNums[xCellNum-1] + yCellNum)[0]; //Keep track of the current cell
+        }
+        else if((xMouseCurrent - xMouseInitial) < -30)//If moving left
+        {
+            xMouseInitial = xMouseCurrent;
+            var newCell = document.getElementsByClassName(horizPosToNums[xCellNum - 2] + yCellNum)[0];
+            newCell.appendChild(copyOfClickedShip);
+            xCellNum--;
+            curCell = document.getElementsByClassName(horizPosToNums[xCellNum-1] + yCellNum)[0]; //Keep track of the current cell
+        }
+
+        //Track y-movement
+        if((yMouseCurrent - yMouseInitial) > 30 && yCellNum < 10-shipLength+1)
+        {
+            yMouseInitial = yMouseCurrent;
+            var newCell = document.getElementsByClassName(horizPosToNums[xCellNum - 1] + (yCellNum+1))[0];
+            newCell.appendChild(copyOfClickedShip);
+            yCellNum++;
+            curCell = document.getElementsByClassName(horizPosToNums[xCellNum-1] + yCellNum)[0]; //Keep track of the current cell
+        }
+        else if((yMouseCurrent - yMouseInitial) < -30 && yCellNum > 1)
+        {
+            yMouseInitial = yMouseCurrent;
+            console.log(document.getElementsByClassName(horizPosToNums[xCellNum - 1] + (yCellNum-1))[0]);
+            console.log(horizPosToNums[xCellNum - 1] + (yCellNum-1));
+            var newCell = document.getElementsByClassName(horizPosToNums[xCellNum - 1] + (yCellNum-1))[0];
+            newCell.appendChild(copyOfClickedShip);
+            yCellNum--;
+            curCell = document.getElementsByClassName(horizPosToNums[xCellNum-1] + yCellNum)[0]; //Keep track of the current cell
+        }
+    }
+    
+    document.onmouseup = function()
+    {
+        document.onmousemove = null;
+        copyOfClickedShip.parentNode.removeChild(copyOfClickedShip);
+        clickedShip.style.display = "block";
+        if(curCell.className != shipParent.className)
+        {
+            curCell.appendChild(clickedShip);
+            curCellLetter = curCell.className.substring(0, 1);
+            curCellNum = parseInt(curCell.className.substring(1));
+        }
+        else
+        {
+            shipParent.appendChild(clickedShip);
+        }
+
+        //Set the ship positions within Model.ships
+        var ship;
+        for(var i = 0; i < model.shipsPlayer.length; i++)
+        {
+            var curShip = model.shipsPlayer[i];
+            if(curShip.id == targetID)
+            {
+                ship = curShip;
+                break;
+            }
+        }
+
+        for(var i = 0; i < shipLength; i++)
+        {
+            ship.locations[i] = curCellLetter + curCellNum;
+            curCellNum++;
+        }
+
+        console.log(model.shipsPlayer);
+    }
 }
+
+
 
